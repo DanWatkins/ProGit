08d853188a86aba7296f41e1c58eef5ccb34e9cb
Dan Watkins <bluehailex@gmail.com>
2013-05-08 17:09:38 -0500

    Added Physics::CalculateAverageDistanceFromOrigin()

--- Src/Physics.h
+++ Src/Physics.h
@@ -17,6 +17,20 @@ public:
 		point.x = x;
 		point.y = y;
 	}
+
+	float CalculateAverageDistanceFromOrigin()
+	{
+		float totalDisplacment = 0.0f;
+
+		std::vector<Point2D<int>>::iterator iter = mPoints.begin();
+		while (iter != mPoints.end())
+		{
+			totalDisplacment += std::sqrtf((float)(iter->x)^2+(float)(iter->y)^2);
+			iter++;
+		}
+
+		return totalDisplacment/mPoints.size();
+	}
 };
 
 #endif
