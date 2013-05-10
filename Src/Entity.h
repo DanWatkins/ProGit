199405dc173b5d44de33a0f27a0c94172de80540
Dan Watkins <bluehailex@gmail.com>
2013-05-10 08:43:20 -0500

    Added Entity::Reset()

--- Src/Entity.h
+++ Src/Entity.h
@@ -12,6 +12,7 @@ private:
 public:
 	Entity();
 	
+	void Reset() { mSpeed = 1; mPos.x = 0; mPos.y = 0; }
 	void Move(double x, double y) { mPos.x += x; mPos.y += y; }
 	int GetSpeed() { return mSpeed; }
 };
